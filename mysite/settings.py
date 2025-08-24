9a73941ce23917e7a90d9fccb3d889934fba0d4b
Manish Prasad Gupta <manishprasad7173@gmail.com>
2025-08-24 23:12:54 +0530

    Implement employee management features with model, views, and templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Media files (User uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from employees.models import Employee
 
 def home(request):
-    return render(request, 'home.html')
+    employees = Employee.objects.all()
+    context = {
+        'employees': employees
+    }
+    return render(request, 'home.html', context)
